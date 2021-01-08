672a83ad1924c55cb13466cd189788ec61ba8167
erpujabaidya <62367545+erpujabaidya@users.noreply.github.com>
2021-01-08 17:48:24 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -3,6 +3,7 @@ provider "aws" {
   
 }
 
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
